cc62ffcabc6d02055fdc8a91e54bcc65ba69cd6e
Ladislav Jaromír Pleštil <48877289+ladislavpl@users.noreply.github.com>
2025-01-12 10:26:17 +0100

    Added kolostesti and README.md

---
+++ kolostesti.py
@@ -0,0 +1,20 @@
+import random
+
+print("Kolo štěstí v1.0\n")
+
+try:
+    while True:
+        kolostesti = []
+        print("Zadej text, který chceš přidat do kola. Pokud jsi skončil, zadej !")
+        while True:
+            temp = input()
+            if temp == "!":
+                break
+            else:
+                kolostesti.append(temp)
+        if kolostesti != []:
+            print("\nVýsledek: " + random.choice(kolostesti) + "\n")
+        else:
+            print("\nNezadali jste žádný text. Kolo nelze spustit!\n")
+except KeyboardInterrupt:
+    exit(0)

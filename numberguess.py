32cd53b52d317ec27deee5cc265dee711242e941
Ladislav Jaromír Pleštil <48877289+ladislavpl@users.noreply.github.com>
2025-01-18 12:15:59 +0100

    updated numberguess.py to release 1.0

--- numberguess.py
+++ numberguess.py
@@ -1,12 +1,12 @@
 import random
 
-print("Number Guessing Game version Beta\n")
+print("Number Guessing Game v1.0\n")
 print("Hádáš číslo mezi 1 - 9.\nNa uhodnutí čísla máš tři pokusy.\nPokud neuhodneš, vygeneruje se číslo nové.\n")
 try:
     while True:
         randomInt = random.randint(1, 9)
-        try:
-            for i in range(3):
+        for i in range(3):
+            try:
                 guess = int(input("Hádej číslo: "))
 
                 if randomInt == guess:
@@ -14,10 +14,12 @@ try:
                     break
                 elif guess >= 10 or guess <= 0:
                     print("Číslo je neplatné! Neplýtvej pokusy. :)\n")
-                else:
-                    print("Číslo jsi neuhodl!\n")
-        except ValueError:
-            print("Zadali jste neplatnou hodnotu!\n")
+                elif guess > randomInt:
+                    print("Vaše číslo je větší než to správné\n")
+                elif guess < randomInt:
+                    print("Vaše číslo je menší než to správné\n") 
+            except ValueError:
+                print("Zadali jste neplatnou hodnotu!\n")   
         print("Generuji nové číslo.\n")
 except KeyboardInterrupt:
     exit(0)

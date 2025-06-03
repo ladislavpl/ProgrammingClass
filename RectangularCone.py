952199adefc73cbb64bbb6abb349fab792ea87ad
Ladislav Jaromír Pleštil <ladaplestil09@seznam.cz>
2025-06-03 10:16:06 +0200

    Added some programs and updated README.md

---
+++ RectangularCone.py
@@ -0,0 +1,13 @@
+print("Volume of rectangular cone v1.0\n")
+
+try:
+    while True:
+        try:
+            a = float(input("Zadejte délku strany a: "))
+            v = float(input("Zadejte výšku jehlanu v: "))
+        except ValueError:
+            print("Zadali jste neplatnou hodnotu!\nHodnota musí být číslo!\n")
+
+        print(f"V = {(1 / 3) * a ** 2 * v}\n")
+except KeyboardInterrupt:
+    exit(0)
